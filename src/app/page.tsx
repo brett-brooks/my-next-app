7d7940bc386dea333101f17cdc9de54e8efe98de
Brett Brooks <brett.brooks@recovery.com>
2025-06-20 12:54:35 -0500

    Added Changeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-'use client'
-import PrimaryButton from "@/components/PrimaryButton";
 import PricingCard from "@/components/PricingCard";
 
 export default function Home() {
   return (
   <main className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
   <div className="max-w-6xl w-full">
-    <h1 className="text-4xl font-bold text-center mb-10">Our Plans</h1>
+    <h1 className="text-4xl font-bold text-center mb-10 text-gray-600">Our Plans</h1>
 
     <div className="grid gap-6 grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
       <PricingCard title="Starter" description="Perfect for hobby projects and testing" price="$0" />
